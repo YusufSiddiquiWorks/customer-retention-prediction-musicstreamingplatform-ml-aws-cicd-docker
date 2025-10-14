9f7ff773710b957434c527d2f49b4820908d6570
Yusuf Siddiqui <57582756+YusufSiddiquiWorks@users.noreply.github.com>
2025-10-14 21:58:39 +0530

    AWS CICD Update

--- scripts/start_containers.sh
+++ scripts/start_containers.sh
@@ -7,6 +7,6 @@ echo "Docker Image "yswork/musicstreaming-customerretention-prediction-ml:aws_pr
 
 echo "Docker Run Starting on Port 7003 "
 
-docker run -d -p 7003:7003 
+docker run -d -p 7003:7003 yswork/musicstreaming-customerretention-prediction-ml:aws_prod_v1
 
 echo "Docker Run Successfully Started on Port 7003 "
