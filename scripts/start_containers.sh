f41d3190a409743701607e28c02614261bdc8dc5
Yusuf Siddiqui <57582756+YusufSiddiquiWorks@users.noreply.github.com>
2025-10-16 02:11:16 +0530

    CodePipeline Update

--- scripts/start_containers.sh
+++ scripts/start_containers.sh
@@ -3,12 +3,12 @@ set -e
 
 echo "Docker Pull Starting"
 
-docker pull yswork/music-customer-churn-prediction-ml:aws_prod_v1
+sudo docker pull yswork/music-customer-churn-prediction-ml:aws_prod_v1
 
 echo "Docker Image "yswork/music-customer-churn-prediction-ml:aws_prod_v1" Pulled Successfully"
 
 echo "Docker Run Starting on Port 7003 "
 
-docker run -d -p 7003:7003 yswork/music-customer-churn-prediction-ml:aws_prod_v1
+sudo docker run -d -p 7003:7003 yswork/music-customer-churn-prediction-ml:aws_prod_v1
 
 echo "Docker Run Successfully Started on Port 7003 "
